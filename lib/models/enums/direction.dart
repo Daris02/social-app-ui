7b9d1f37440e13dce6b890ba18b71b84d8506897
Daris02 <hei.raymond.2@gmail.com>
2025-10-30 09:08:10 +0300

    feat: add description to enum type

--- lib/models/enums/direction.dart
+++ lib/models/enums/direction.dart
@@ -1,2 +1,24 @@
-// ignore: constant_identifier_names
-enum Direction { DD, DF, DLP, DLE, DARH, DSIC }
+enum Direction {
+  DD('DD', 'Direction de la Décentralisation'),
+  DF('DF', 'Direction Financière'),
+  DLP('DLP', 'Direction de la Logistique et du Patrimoine'),
+  DLE('DLE', 'Direction de la Législation et des Etudes'),
+  DARH('DARH', 'Direction Administrative et des Ressources Humaines'),
+  DSIC('DSIC', 'Direction du Système d\'Information et de la Communication');
+
+  final String code;
+  final String description;
+
+  const Direction(this.code, this.description);
+
+  @override
+  String toString() => code;
+
+  static Direction? fromCode(String code) {
+    try {
+      return Direction.values.firstWhere((e) => e.code == code);
+    } catch (_) {
+      return null;
+    }
+  }
+}
