ed338fe8f10298a7b7928645298702a295b5a447
Daris02 <hei.raymond.2@gmail.com>
2026-06-29 13:50:22 +0300

    chore: update Android Gradle plugin version to 8.9.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.social_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "29.0.14206865" // "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
